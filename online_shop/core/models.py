b9bb4b3171b3d171df5e99e9421b02d3400f9526
FrolovSergey96 <112880329+FrolovSergey96@users.noreply.github.com>
2023-07-23 21:08:56 +0500

    templates_html

--- online_shop/core/models.py
+++ online_shop/core/models.py
@@ -1,3 +1,87 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    title = models.CharField(verbose_name='Name', max_length=64)
+    description = models.TextField('Description', max_length=255)
+    price = models.DecimalField(decimal_places=2)
+
+"""
+Пример:
+
+# title: название кофе
+# id: уникальный идентификатор (задается автоматически)
+# Описание объекта кофе (поля: id, название)
+
+
+class Coffee(models.Model):
+    title = models.CharField(
+        max_length=64,
+        verbose_name='Название'
+    )
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Кофе'
+        verbose_name_plural = 'Кофе'
+        
+class Price(models.Model):
+    price = models.PositiveIntegerField(
+        verbose_name = 'Цена'
+    )
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Цена'
+        verbose_name_plural = 'Цены'
+
+class Volume(models.Model):
+    title = models.CharField(
+        max_length=32,
+        verbose_name='Объем'
+    )
+    
+    price = models.OneToOneField(
+        'Price',
+        verbose_name='Цена кофе',
+        on_delete=models.CASCADE
+    )
+    
+    def __str__(self):
+        return self.title
+        
+    class Meta:
+        verbose_name = 'Объём'
+        verbose_name_plural = 'Объёмы'
+        
+class Topping(models.Model):
+    title = models.CharField(
+        max_length=64,
+        verbose_name='Сироп'
+    )
+    
+    price = models.ForeignKey(
+        'Price',
+        verbose_name='Цена сиропа',
+        on_delete=models.CASCADE
+    )    
+    
+    def __str__(self):
+    return self.title
+        
+    class Meta:
+        verbose_name = 'Сироп'
+        verbose_name_plural = 'Сиропы'
+
+class Profile(models.Model):
+    pass
+    
+class Order(models.Model):
+    pass
+    
+"""
+
--- online_shop/online_shop/urls.py
+++ online_shop/online_shop/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # связываем пути(ссылки) прописанные в "ядре" с "основой" проекта
+    path('', include('core.urls'))
 ]
